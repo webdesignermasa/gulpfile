3feca365537cda5add6bb1cbd0bacc233f538575
masa <webdesignermasa@gmail.com>
2021-11-07 15:18:37 +0900

    ブラウザのオートリロードに対応

--- gulpfile.js
+++ gulpfile.js
@@ -1,27 +1,45 @@
 'use strict';
 
+// Gulp・通知
 const { src, dest, watch, series, parallel, lastRun } = require('gulp');
 const plumber = require('gulp-plumber');
 const notify = require('gulp-notify');
+
+// CSS・Sass
 const sassGlob = require('gulp-sass-glob');
 const sass = require('gulp-sass')(require('sass'));
 const postcss = require('gulp-postcss');
 const autoprefixer = require('autoprefixer');
+
+// JavaScript
 const babel = require('gulp-babel');
 const uglify = require('gulp-uglify');
 const rename = require('gulp-rename');
+
+// 画像削除
 const del = require('del');
+
+// 画像圧縮
 const imagemin = require('gulp-imagemin');
 const imageminGifsicle = require('imagemin-gifsicle');
 const imageminMozjpeg = require('imagemin-mozjpeg');
 const imageminPngquant = require('imagemin-pngquant');
 const imageminSvgo = require('imagemin-svgo');
 
+// ブラウザオートリロード
+const browserSync = require('browser-sync').create();
+
+// HTMLをコピーする
 function htmlCopy() {
   return src('src/**/*.html')
-    .pipe(dest('dist'));
+    .pipe(dest('dist'))
+    .pipe(notify({
+      message: 'Copied HTML',
+      onLast: true,
+    }));
 }
 
+// Sassをコンパイルする
 function cssTranspile() {
   return src('src/sass/**/*.scss')
     .pipe(plumber({
@@ -37,6 +55,7 @@ function cssTranspile() {
     }));
 };
 
+// JavaScriptをコンパイル＆圧縮（難読化）する
 function jsTranspile() {
   return src('src/js/**/*.js')
     .pipe(plumber({
@@ -50,15 +69,25 @@ function jsTranspile() {
     .pipe(rename({
       extname: '.min.js',
     }))
-    .pipe(dest('dist/js'));
+    .pipe(dest('dist/js'))
+    .pipe(notify({
+      message: 'Compiled JavaScript',
+      onLast: true,
+    }));
 }
 
+// 画像を削除する
 function imageClean() {
-  return del(['dist/img']);
+  return del(['dist/img'])
+    .pipe(notify({
+      message: 'Deleted Images',
+      onLast: true,
+    }));
 }
 
-function imageMinify() {
-  return src('src/img/**/*', { since: lastRun(imageMinify) })
+// 画像を圧縮する
+function imageCompress() {
+  return src('src/img/**/*', { since: lastRun(imageCompress) })
     .pipe(plumber({
       errorHandler: notify.onError('Error: <%= error.message %>'),
     }))
@@ -77,19 +106,49 @@ function imageMinify() {
       verbose: true
     }
     ))
-    .pipe(dest('dist/img'));
+    .pipe(dest('dist/img'))
+    .pipe(notify({
+      message: 'Compressed Images',
+      onLast: true,
+    }));
 }
 
+// ブラウザのオートリロード初期化する
+function browserSyncInit() {
+  browserSync.init({
+    server: {
+      baseDir: 'dist',
+    },
+  });
+}
+
+// ブラウザをオートリロードする
+function browserSyncReload(callback) {
+  browserSync.reload();
+  callback();
+}
+
+// ファイルの変更を監視する
 function watchFiles() {
-  watch('src/**/*.html', htmlCopy);
-  watch('src/sass/**/*.scss', cssTranspile);
-  watch('src/js/**/*.js', jsTranspile);
-  watch('src/img/**/*', imageMinify);
+  watch('src/**/*.html', series(htmlCopy, browserSyncReload));
+  watch('src/sass/**/*.scss', series(cssTranspile, browserSyncReload));
+  watch('src/js/**/*.js', series(jsTranspile, browserSyncReload));
+  watch('src/img/**/*', series(imageCompress, browserSyncReload));
 }
 
-exports.htmlCopy = htmlCopy;
-exports.cssTranspile = cssTranspile;
-exports.jsTranspile = jsTranspile;
-exports.imagemin = series(imageClean, imageMinify);
-
-exports.default = watchFiles;
+// Gulpタスク
+exports.html = htmlCopy;
+exports.css = cssTranspile;
+exports.js = jsTranspile;
+exports.img = imageCompress;
+exports.imgAll = series(
+  imageClean, imageCompress
+);
+exports.build = series(
+  htmlCopy, cssTranspile, jsTranspile, imageCompress
+);
+exports.watch = watchFiles;
+exports.default = series(
+  htmlCopy, cssTranspile, jsTranspile, imageCompress,
+  parallel(browserSyncInit, watchFiles)
+);
