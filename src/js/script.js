ccde2b00f977b19cf8f16ec9da05167469b9bd71
masa <webdesignermasa@gmail.com>
2021-11-13 17:41:49 +0900

    ハンバーガーボタンを追加

--- src/js/script.js
+++ src/js/script.js
@@ -1,6 +1,18 @@
 jQuery(function($) {
 
-  const dummy = () => {
-    return 0;
-  }
+  // ハンバーガーメニュー
+  $('.js-burger-btn').click(function() {
+    $('.js-burger-btn').toggleClass('is-open');
+    $('.js-burger-menu').toggleClass('is-open');
+    $('html, body').toggleClass('is-fixed');
+
+    // スクロール禁止（iOSでは追加設定が必要）
+    if ($('html').hasClass('is-fixed')) {
+      $(window).on('touchmove.noScroll', function(e) {
+        e.preventDefault();
+      });
+    } else {
+      $(window).off('.noScroll');
+    }
+  });
 });
